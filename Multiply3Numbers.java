ab244d662a2467c606cfef2c0fb793498130f1e3
rdtzer <161980609+rdtzer@users.noreply.github.com>
2025-04-24 23:01:30 -0700

    Add files via upload

---
+++ Multiply3Numbers.java
@@ -0,0 +1,15 @@
+package day3;
+
+public class Multiply3Numbers {
+
+	public static void main(String[] args) 
+	{
+	int num1= 3;
+	int num2= 6;
+	int num3= 9;
+	int product;
+	product = num1*num2*num3; 
+	System.out.println(product);
+	}
+
+}
